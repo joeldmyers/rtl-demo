badbd37c517090e9fa2f730733d505e16278e643
Joel Myers <joeldmyers@gmail.com>
2022-02-01 10:56:48 -0500

    simple app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-  return (
+const App = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [notes, setNotes] = useState("");
+  const [showErrors, setShowErrors] = useState(false);
+  const [submitIsSuccessful, setSubmitIsSuccessful] = useState(false);
+
+  const handleSubmit = () => {
+    console.log("submitting");
+    if (!firstName || !lastName || !email || !notes) {
+      setShowErrors(true);
+      return;
+    }
+    setShowErrors(false);
+
+    setSubmitIsSuccessful(true);
+  };
+
+  const SuccessMessage = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="success-message">Success!</div>
     </div>
   );
-}
+
+  if (submitIsSuccessful) return <SuccessMessage />;
+
+  return (
+    <div className="App">
+      <div className="form">
+        <label htmlFor="fname">First name:</label>
+        <input
+          type="text"
+          id="fname"
+          name="fname"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        {showErrors && !firstName && (
+          <span className="error">Please enter a first name</span>
+        )}
+        <label htmlFor="lname">Last name:</label>
+        <input
+          type="text"
+          id="lname"
+          name="lname"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        {showErrors && !lastName && (
+          <span className="error">Please enter a last name</span>
+        )}
+        <label htmlFor="lname">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {showErrors && !email && (
+          <span className="error">Please enter an email</span>
+        )}
+        <label htmlFor="lname">Notes:</label>
+        <input
+          type="text"
+          id="notes"
+          name="notes"
+          value={notes}
+          onChange={(e) => setNotes(e.target.value)}
+        />
+        {showErrors && !notes && (
+          <span className="error">Please enter notes</span>
+        )}
+        <button type="submit" value="Submit" onClick={handleSubmit}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
